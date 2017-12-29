ab86ea59b2a155dea5455dd4a28d370218ebd1c0
Daniela Simões <darny_10@hotmail.com>
2017-12-29 22:51:14 +0000

    Validate chain

--- client/client_cc.py
+++ client/client_cc.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 import os
 import pkcs11
 from cryptography import x509
@@ -7,9 +8,16 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding as _aspaadding
 import getpass
+from OpenSSL import crypto
+from pem import parse_file
 
 
 class CitizenCard:
+    """
+    - Deciphering using the Private Key of the Portuguese Citizen Card is not currently supported, the code
+    to be used: https://github.com/danni/python-pkcs11/blob/master/docs/opensc.rst
+
+    """
 
     def __init__(self, pin=None):
         if pin is None:
@@ -32,7 +40,11 @@ class CitizenCard:
             return priv.sign(data, mechanism=pkcs11.Mechanism.SHA256_RSA_PKCS)
 
     def decrypt(self, data):
-        pass
+        with self.token.open(user_pin=self.pin) as session:
+            priv = session.get_key(pkcs11.constants.ObjectClass.PRIVATE_KEY,
+                                   pkcs11.KeyType.RSA, 'CITIZEN AUTHENTICATION KEY')
+
+            return priv.decrypt(data, mechanism=pkcs11.Mechanism.SHA256_RSA_PKCS)
 
     def get_certificate(self, label="CITIZEN AUTHENTICATION CERTIFICATE"):
         with self.token.open(user_pin=self.pin) as session:
@@ -62,6 +74,19 @@ class CitizenCard:
                 format=serialization.PublicFormat.SubjectPublicKeyInfo
             )
 
+    def get_available_certs_labels(self):
+        certs_labels = []
+
+        with self.token.open(user_pin=self.pin) as session:
+            # get public key certificates
+            for cert in session.get_objects({pkcs11.constants.Attribute.CLASS:
+                                             pkcs11.constants.ObjectClass.CERTIFICATE}):
+
+                label = cert[pkcs11.constants.Attribute.LABEL]
+                certs_labels.append(label)
+
+        return certs_labels
+
     def verify(self, message, sign_bytes, x509_pem):
         cert = x509.load_pem_x509_certificate(x509_pem, default_backend())
         # Extract public key from certificate
@@ -79,7 +104,50 @@ class CitizenCard:
         return verifier.verify()
 
     def encrypt(self, message, x509_pem):
-        pass
+        cert = x509.load_pem_x509_certificate(x509_pem, default_backend())
+        # Extract public key from certificate
+        sign_cert_pk = cert.public_key()
+
+        # encrypt message
+        return sign_cert_pk.encrypt(message, _aspaadding.OAEP(
+                                       mgf=_aspaadding.MGF1(algorithm=hashes.SHA256()),
+                                       algorithm=hashes.SHA256(),
+                                       label=None
+                                    ))
+
+    def validate_chain(self, chain, pem_certificate, ssl_ca_root_file="./mozilla-ca-bundle.txt"):
+        # parse CA roots certificate PEMs to an list
+        trusted_certs_pems = parse_file(ssl_ca_root_file)
+
+        # create a new store
+        store = crypto.X509Store()
+
+        # check middle CAs for revocation
+        store.set_flags(crypto.X509StoreFlags.CRL_CHECK)
+
+        # check just the certificate CRL and not if all certificates up to the root are revoked
+        # not recommended since requires all CAs root revogations
+        store.set_flags(crypto.X509StoreFlags.CRL_CHECK_ALL)
+
+        # add system trusted CA roots to store
+        for pem_crt in trusted_certs_pems:
+            store.add_cert(crypto.load_certificate(crypto.FILETYPE_PEM, pem_crt.as_bytes()))
+
+        # load supplied chain
+        for pem_crt in chain:
+            store.add_cert(crypto.load_certificate(crypto.FILETYPE_PEM, pem_crt))
+
+        # convert pem to OpenSSL certificate format
+        certificate = crypto.load_certificate(crypto.FILETYPE_PEM, pem_certificate)
+
+        # validate full chain
+        store_ctx = crypto.X509StoreContext(store, certificate)
+
+        # load CRLs to the store
+        for crl in [f for f in os.listdir("crls") if os.path.isfile(os.path.join("crls", f))]:
+            store.add_crl(crypto.load_crl(crypto.FILETYPE_PEM, open(os.path.join("crls", crl), "r").read()))
+
+        store_ctx.verify_certificate()
 
 
 if __name__ == '__main__':
@@ -91,6 +159,15 @@ if __name__ == '__main__':
     # x509 PEM CERTIFICATE
     x509_pem = cc.get_certificate_pem()
     cc.verify(data, signature, x509_pem)
+
+    # encrypt data
+    x509_pem = cc.get_certificate_pem()
+    data_ciphered = cc.encrypt(data, x509_pem)
+
+    # cc.decrypt(data_ciphered)
+    x509_chain = [cc.get_certificate_pem(label=label) for label in cc.get_available_certs_labels()]
+
+    cc.validate_chain(chain=x509_chain, pem_certificate=x509_pem)
 
     print(cc.get_certificate_pem())
     print(cc.get_public_key_pem())
